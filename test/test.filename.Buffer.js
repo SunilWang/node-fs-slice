0f1f71fb7acd89f3973711a63902ae82da8b9814
SunilWang <ahwangshu@qq.com>
2018-12-06 20:56:59 +0800

    2.x done

---
+++ test/test.filename.Buffer.js
@@ -0,0 +1,141 @@
+import test from 'ava'
+import fs from 'fs'
+import fss from '../index'
+
+const IMAGE_FILENAME = './test/data/image.jpg'
+const TEXT_FILENAME = './test/data/text'
+
+let fsImage = null
+let fsText = null
+
+function streamToBuffer (path) {
+  let stream = fs.createReadStream(path)
+  return new Promise((resolve, reject) => {
+    let buffers = []
+    stream.on('error', reject)
+    stream.on('data', (data) => buffers.push(data))
+    stream.on('end', () => resolve(Buffer.concat(buffers)))
+  })
+}
+
+test.before('init', async function () {
+  let imgeBuffer = await streamToBuffer(IMAGE_FILENAME)
+  let textBuffer = await streamToBuffer(TEXT_FILENAME)
+  fsImage = fss(imgeBuffer, { blockSize: 204800 })
+  fsText = fss(textBuffer, { blockSize: 204800 })
+})
+
+test.after('close', () => {
+  fsImage = null
+  fsText = null
+})
+
+test('should slice success', async function (t) {
+  let readable = fsImage.slice()
+  let tempFilename = './test/temp/buffer/slice_temp.jpg'
+
+  return new Promise(function (resolve, reject) {
+    let writable = fs.createWriteStream(tempFilename)
+
+    writable.on('finish', () => {
+      return resolve()
+    })
+
+    writable.on('error', (err) => {
+      return reject(err)
+    })
+
+    readable.on('error', (err) => {
+      return reject(err)
+    })
+
+    readable.pipe(writable)
+  }).then(function () {
+    t.is(fs.statSync(tempFilename).size, 204800)
+    fs.unlinkSync(tempFilename)
+  })
+})
+
+test('should sliceAsFile success', function * (t) {
+  let tempFilename = './test/temp/buffer/sliceAsFile_temp.jpg'
+  yield fsImage.sliceAsFile(tempFilename, {start: 0, end: 500000})
+
+  t.is(fs.statSync(tempFilename).size, 500000)
+  fs.unlinkSync(tempFilename)
+})
+
+test('should sliceAsFile by text success', function * (t) {
+  let tempFilename = './test/temp/buffer/sliceAsFile_text_temp'
+
+  yield fsText.sliceAsFile(tempFilename, {start: 0, end: 10})
+  let data = fs.readFileSync(tempFilename, 'utf-8')
+
+  t.is(data, "A\nA's\nAA's")
+  t.is(fs.statSync(tempFilename).size, 10)
+
+  fs.unlinkSync(tempFilename)
+})
+
+test('should sliceAsFile default interval success', function * (t) {
+  let tempFilename = './test/temp/buffer/sliceAsFile_default_interval_temp.jpg'
+
+  yield fsImage.sliceAsFile(tempFilename)
+
+  t.is(fs.statSync(tempFilename).size, 204800)
+
+  fs.unlinkSync(tempFilename)
+})
+
+test('should avgSliceAsFile success', function * (t) {
+  let files = yield fsImage.avgSliceAsFile()
+
+  t.is(files.length, 3)
+  t.is(fs.statSync(files[0]).size, 204800)
+  t.is(fs.statSync(files[1]).size, 204800)
+  t.is(fs.statSync(files[2]).size, 107343)
+
+  for (let file of files) {
+    fs.unlinkSync(file)
+  }
+})
+
+test('should join success', function * (t) {
+  let tempFilename = './test/temp/buffer/join_temp'
+  let res = yield fsText.avgSliceAsFile()
+  let writable = fs.createWriteStream(tempFilename)
+
+  yield fsText.join(res, writable)
+
+  t.is(fs.statSync(TEXT_FILENAME).size, fs.statSync(tempFilename).size)
+
+  let textData = fs.readFileSync(TEXT_FILENAME, 'utf-8')
+  let tempData = fs.readFileSync(tempFilename, 'utf-8')
+
+  t.is(textData, tempData)
+
+  fs.unlinkSync(tempFilename)
+
+  for (let file of res) {
+    fs.unlinkSync(file)
+  }
+})
+
+test('should joinAsFile success', function * (t) {
+  let tempFilename = './test/temp/buffer/joinAsFile_temp'
+  let res = yield fsText.avgSliceAsFile()
+
+  yield fsText.joinAsFile(res, tempFilename)
+
+  t.is(fs.statSync(TEXT_FILENAME).size, fs.statSync(tempFilename).size)
+
+  let textData = fs.readFileSync(TEXT_FILENAME, 'utf-8')
+  let tempData = fs.readFileSync(tempFilename, 'utf-8')
+
+  t.is(textData, tempData)
+
+  fs.unlinkSync(tempFilename)
+
+  for (let file of res) {
+    fs.unlinkSync(file)
+  }
+})
